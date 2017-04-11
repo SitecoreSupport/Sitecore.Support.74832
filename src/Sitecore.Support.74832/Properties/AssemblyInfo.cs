8a49aaf265a640da9295f5775a0248da68ce4280
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2017-04-11 10:47:58 +0800

    Initial commit

---
+++ src/Sitecore.Support.74832/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.74832")]
+[assembly: AssemblyProduct("Sitecore.Support.74832")]
+[assembly: ComVisible(false)]
