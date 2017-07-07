bc844b7b6a203bcf63d4ac992f6d8f7bd20ed1dc
Anton Kechashin <atk@sitecore.net>
2017-07-07 23:11:50 +0300

    Version for EXM 3.2.1.

---
+++ src/Sitecore.Support.74832/Modules/EmailCampaign/Core/Pipelines/RedirectUrl/IdentifyContact.cs
@@ -0,0 +1,56 @@
+﻿namespace Sitecore.Support.Modules.EmailCampaign.Core.Pipelines.RedirectUrl
+{
+  using Sitecore.Analytics;
+  using Sitecore.Analytics.Model;
+  using Sitecore.Diagnostics;
+  using Sitecore.Modules.EmailCampaign.Core.Pipelines.RedirectUrl;
+  using Sitecore.Modules.EmailCampaign.Recipients;
+  using Sitecore.Modules.EmailCampaign.Xdb;
+  using System;
+
+  public class IdentifyContact
+  {
+    public void Process(RedirectUrlPipelineArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      if (!Tracker.IsActive)
+      {
+        try
+        {
+          Tracker.StartTracking();
+        }
+        catch (Exception)
+        {
+          return;
+        }
+      }
+      if ((Tracker.Current != null) && (Tracker.Current.Session != null))
+      {
+        try
+        {
+          if (args.RecipientId != null)
+          {
+            Type[] requestedPropertyTypes = new Type[] { typeof(XdbRelation) };
+            Recipient recipientSpecific = RecipientRepository.GetDefaultInstance().GetRecipientSpecific(args.RecipientId, requestedPropertyTypes);
+            if (recipientSpecific != null)
+            {
+              XdbRelation defaultProperty = recipientSpecific.GetProperties<XdbRelation>().DefaultProperty;
+              if (((defaultProperty != null) && !string.IsNullOrEmpty(defaultProperty.Identifier)) && ((Tracker.Current.Contact == null) || (Tracker.Current.Contact.Identifiers.Identifier != defaultProperty.Identifier)))
+              {
+                Tracker.Current.Session.Identify(defaultProperty.Identifier);
+              }
+            }
+          }
+          if (Tracker.Current.Contact != null)
+          {
+            Tracker.Current.Contact.Identifiers.IdentificationLevel = ContactIdentificationLevel.Known;
+            Tracker.Current.Contact.ContactSaveMode = ContactSaveMode.AlwaysSave;
+          }
+        }
+        catch (Exception)
+        {
+        }
+      }
+    }
+  }
+}
